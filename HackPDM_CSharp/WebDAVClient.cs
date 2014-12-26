13ec28c0adcdf85253c7948b0d3bb42b7e94e08c
matt454357 <matt454357@gmail.com>
2014-12-26 10:52:29 -0700

    fixed license statement

--- HackPDM_CSharp/WebDAVClient.cs
+++ HackPDM_CSharp/WebDAVClient.cs
@@ -3,20 +3,20 @@
  * (C) 2013 Matt Taylor
  * Date: 2/18/2013
  * 
- * This file is part of Foobar.
+ * This file is part of HackPDM.
  * 
- * Foobar is free software: you can redistribute it and/or modify
+ * HackPDM is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * Foobar is distributed in the hope that it will be useful,
+ * HackPDM is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * along with HackPDM.  If not, see <http://www.gnu.org/licenses/>.
  * 
  * 
  * Inspired by Kees van den Broek: kvdb@kvdb.net
