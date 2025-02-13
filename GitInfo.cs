1108619bb796d052ec9f33dc8513ae6918285920
moonxbears <60631427+moonxbears@users.noreply.github.com>
2025-02-13 15:59:42 -0700

    Automated commit from build

--- GitInfo.cs
+++ GitInfo.cs
@@ -1,2 +1,2 @@
-[assembly: System.Reflection.AssemblyMetadata("CommitHash", "18a507e65eaa96f48c89598fe4f49018e131735b")]
-[assembly: System.Reflection.AssemblyMetadata("CommitDate", "2025-02-11 16:55:30 -0700")]
+[assembly: System.Reflection.AssemblyMetadata("CommitHash", "7013cf60fa9ebfb924f250619c13cdcf25284d4a")]
+[assembly: System.Reflection.AssemblyMetadata("CommitDate", "2025-02-13 15:56:42 -0700")]
