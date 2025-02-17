859086e668f5c1b4f744cd868bb271aed831ff0f
moonxbears <60631427+moonxbears@users.noreply.github.com>
2025-02-17 11:25:16 -0700

    Automated commit from build

--- HackPDM_CSharp/GitInfo.cs
+++ HackPDM_CSharp/GitInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
 
-[assembly: AssemblyInformationalVersion("Commit: c2b08d8397824803756d88e9f399d2585750f166, Date: 2025-02-17 09:14:18 -0700")]
+[assembly: AssemblyInformationalVersion("Commit: d24427419d3de7e4729c720ad58ea1c1b5f8e95f, Date: 2025-02-17 11:10:14 -0700")]
