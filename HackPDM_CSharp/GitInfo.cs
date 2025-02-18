972c9ac34e6be3dbcfc5f61d34b43f371821c7e3
Justin Johnson <jnjohnson@ZIP.AZI>
2025-02-18 07:06:00 -0700

    Automated commit from build

--- HackPDM_CSharp/GitInfo.cs
+++ HackPDM_CSharp/GitInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
 
-[assembly: AssemblyInformationalVersion("Commit: b9b14a61384f1354ca2660a3e105de7439700434, Date: 2025-02-17 16:34:26 -0700")]
+[assembly: AssemblyInformationalVersion("Commit: 3fa5b3c9b14cdf11fe81b36978e37abd43c1dcbc, Date: 2025-02-17 16:59:56 -0700")]
